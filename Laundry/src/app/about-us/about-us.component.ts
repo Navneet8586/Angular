b1d83d3850e7c929b73533feb991364aa64962e0
Navneet Kumar <jhanavneet8586@gmail.com>
2024-06-16 00:13:21 +0530

    done

--- Laundry/src/app/about-us/about-us.component.ts
+++ Laundry/src/app/about-us/about-us.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { contact } from '../model';
 import { services } from '../model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,17 +10,28 @@ import { services } from '../model';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent implements OnInit {
+  contactForm!:FormGroup;
   contactDetails=contact;
   services=services;
-
-  
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
   lat: number = 51.673858;
   lng: number = 7.815982;
+
+  
+  constructor(private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm=this.fb.group({
+      name:['',Validators.required],
+      email:['',Validators.required],
+      phone:['',Validators.required],
+      selectedService:['',Validators.required],
+      message:['',Validators.required]
+    })
+  }
+
+  submit(){
+    console.log(this.contactForm.value);
+  }
 
   
   
